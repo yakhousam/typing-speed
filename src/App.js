ff33c971ae778f19f102b0688f0da85be9088b6f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-10 20:06:10 +0100

    added bold lettre when user tape

--- src/App.js
+++ src/App.js
@@ -2,37 +2,52 @@ import React, { useReducer, useEffect } from "react";
 import "./App.css";
 import { initState } from "./state";
 import reducer from "./reducer";
-import { setInput, updateDisplayTxt, incrementCursor, setError, removeError } from "./actions";
-import {checkInput} from './utils'
+import {
+  setInput,
+  updateDisplayTxt,
+  incrementCursor,
+  addError,
+  removeError
+} from "./actions";
+import { checkInput } from "./utils";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  font-size: 20px;
+    font-family: monospace;
+  }`;
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initState);
   const { input, cursor, displayText, textArr, errorArr } = state;
   useEffect(() => {
-    updateDisplayTxt(cursor, dispatch);
-  }, [cursor, errorArr]);
+    updateDisplayTxt(dispatch);
+  }, [cursor, errorArr, input]);
   return (
     <>
+      <GlobalStyle />
       <div>{displayText}</div>
       <input
         type="text"
         value={input}
         onChange={e => {
           let input = e.target.value;
-          if(!checkInput(input, textArr[cursor])){
-            console.log('---wrong---')
-            if(!errorArr.includes(cursor)){
-              setError(cursor, dispatch)
+          if (!checkInput(input, textArr[cursor])) {
+            console.log("---wrong---");
+            if (!errorArr.includes(cursor)) {
+              addError(dispatch);
             }
-          }else{
-            if(errorArr.includes(cursor)){
-              removeError(cursor, dispatch)
+          } else {
+            if (errorArr.includes(cursor)) {
+              removeError(dispatch);
             }
           }
-          if (input.endsWith(' ')){
+          if (input.endsWith(" ")) {
             incrementCursor(dispatch);
-            input = ""
-          } 
+            input = "";
+          }
           setInput(input, dispatch);
         }}
       />
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,52 @@
-export const checkInput = (input='', str='') => {
-  if(input.trim() !== str.slice(0, input.length)){
-    return false
+import React from "react";
+import { Word } from "./styledComponents";
+
+export const checkInput = (input = "", str = "") => {
+  if (input.trim() !== str.slice(0, input.length)) {
+    return false;
   }
-  return true
-}
+  return true;
+};
+
+export const formatDisplayTxt = state => {
+  const { cursor, errorArr, input, textArr } = state;
+  if (cursor > textArr.length) return state.displayText;
+  let currentWord;
+  if (!input) {
+    currentWord = (
+      <Word errStyle={errorArr.includes(cursor)} selected key={cursor}>
+        {textArr[cursor]}
+      </Word>
+    );
+  } else {
+    currentWord = (
+      <Word errStyle={errorArr.includes(cursor)} selected key={cursor}>
+        {textArr[cursor].slice(0, input.length - 1)}{" "}
+        <span style={{ fontWeight: "bold", margin: "-.5em" }}>
+          {textArr[cursor].charAt(input.length - 1)}
+        </span>{" "}
+        {textArr[cursor].slice(input.length)}
+      </Word>
+    );
+  }
+  if (cursor === 0) {
+    return [currentWord, ...state.displayText.slice(1)];
+  }
+  if (cursor === 1) {
+    return [
+      <Word errStyle={errorArr.includes(0)} key={0}>
+        {state.textArr[0]}
+      </Word>,
+      currentWord,
+      ...state.displayText.slice(2)
+    ];
+  }
+  return [
+    ...state.displayText.slice(0, cursor - 1),
+    <Word errStyle={errorArr.includes(cursor - 1)} key={cursor - 1}>
+      {state.textArr[cursor - 1]}
+    </Word>,
+    currentWord,
+    ...state.displayText.slice(cursor + 1)
+  ];
+};
