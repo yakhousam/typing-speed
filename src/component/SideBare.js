3be12d1e5a20988c592215ba6254573682bbbf67
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-15 18:52:41 +0100

    styled the score component

--- src/component/SideBare.js
+++ src/component/SideBare.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { SideBare as Aside, Section } from "./styledComponents";
+import { SideBare as Aside, ScoreContainer } from "./styledComponents";
 
-const SideBare = ({score, accuracy}) => {
+const SideBare = ({ score, accuracy }) => {
   return (
     <Aside>
-      <Section justify="space-around">
-        <div>WPM: {score}</div>
-        <div>Accuracy: {accuracy}</div>
-      </Section>
+      <ScoreContainer>
+        <div style={{ fontSize: "72px" }}>
+          <p>WPM</p>
+          <p>{score}</p>
+        </div>
+        <div style={{ fontSize: "24px", color: 'orange' }}>
+          Accuracy: <span>{accuracy}</span>{" "}
+        </div>
+      </ScoreContainer>
     </Aside>
   );
 };
