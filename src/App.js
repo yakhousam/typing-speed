7f05ecc4d776a53d0c90764aec940d0e6ee16f72
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-15 18:48:05 +0100

    added global style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, {useState} from "react";
-import "./App.css";
+import {createGlobalStyle} from 'styled-components'
+
 
 import { Grid } from "./component/styledComponents";
 import MainPage from "./component/MainPage";
 import SideBare from "./component/SideBare";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+  }
+  html, body{
+    margin: 0;
+    padding: 0;
+  }
+`
 
 
 function App() {
 const [score, setScore] = useState(0)
 const [accuracy, setAccuracy] = useState(100)
   return (
+    <>
+    <GlobalStyle />
     <Grid>
       <MainPage setScore={setScore} setAccuracy={setAccuracy} />
       <SideBare score={score} accuracy={accuracy} />
     </Grid>
+    </>
   );
 }
 
