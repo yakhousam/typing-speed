6a24a6621d2a4093e7e3e2e0a06b5c4bf5d98bc2
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-17 18:27:54 +0100

    added TextBox component

---
+++ src/component/TextBox.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from "react";
+import { StyledTextBox } from "./styledComponents";
+import { updateToolTip, updateCurrentWordOffset } from "../actions";
+import { getOffsetTop } from "../utils";
+
+function TextBox(props) {
+  const {
+    toolTip,
+    errorArrTxt,
+    errorArr,
+    input,
+    currentWordOffsetTop,
+    cursor,
+    displayText
+  } = props.state;
+  const { dispatch } = props;
+  const textBoxRef = useRef();
+  useEffect(() => {
+    if (cursor === 0) {
+      textBoxRef.current.scroll(0, 0);
+    }
+  }, [cursor]);
+  useEffect(() => {
+    if (input === "") {
+      const offset = getOffsetTop(textBoxRef);
+      if (offset > currentWordOffsetTop) {
+        if (currentWordOffsetTop > 0) {
+          textBoxRef.current.scroll({
+            top: textBoxRef.current.scrollTop + (offset - currentWordOffsetTop),
+            behavior: "smooth"
+          });
+        }
+        updateCurrentWordOffset(dispatch, offset);
+      }
+    }
+  }, [input, currentWordOffsetTop, dispatch]);
+  return (
+    <StyledTextBox
+      id="textBox"
+      ref={textBoxRef}
+      height="5em"
+      onMouseOut={() => {
+        updateToolTip(dispatch, { ...toolTip, txt: "", visible: false });
+      }}
+      onMouseOver={e => {
+        const { id } = e.target;
+        const error = errorArrTxt.find(el => el.id === +id);
+        if (error) {
+          updateToolTip(dispatch, {
+            ...toolTip,
+            visible: true,
+            txt: error.input
+          });
+        } else if (errorArr.includes(+id)) { 
+          updateToolTip(dispatch, {
+            ...toolTip,
+            visible: true,
+            txt: ""
+          });
+        }
+      }}
+      onMouseMove={e => {
+        const left = e.clientX;
+        const top = e.clientY;
+        if (toolTip.visible) {
+          updateToolTip(dispatch, { ...toolTip, left, top });
+        }
+      }}
+    >
+      {displayText}
+    </StyledTextBox>
+  );
+}
+
+export default TextBox;
