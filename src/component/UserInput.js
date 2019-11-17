c62904dcfe01978ba06c5ab94837da0f4a6e7102
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-17 18:29:02 +0100

    added UserInput component

---
+++ src/component/UserInput.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  setInput,
+  incrementCursor,
+  addError,
+  addErrorTxt,
+  removeError,
+  startTimer
+} from "../actions";
+import { checkInput } from "../utils";
+
+const UserInput = (props) =>  {
+  const { dispatch } = props;
+  const {
+    textArr,
+    isTimerStarted,
+    timer,
+    cursor,
+    input,
+    errorArr
+  } = props.state;
+  const handleChange = e => {
+    if (timer < 1 || cursor > textArr.length - 1) {
+      return;
+    }
+    if (!isTimerStarted && timer > 0) {
+      startTimer(true, dispatch);
+    }
+    let input = e.target.value;
+    if (!checkInput(input, textArr[cursor])) {
+      if (!errorArr.includes(cursor)) {
+        addError(dispatch, input);
+      }
+    } else {
+      if (errorArr.includes(cursor)) {
+        removeError(dispatch);
+      }
+    }
+    if (input.endsWith(" ")) {
+      if (errorArr.includes(cursor)) {
+        addErrorTxt(dispatch, { id: cursor, input });
+      }
+      incrementCursor(dispatch);
+      input = "";
+    }
+    setInput(input, dispatch);
+  };
+  return <StyledInput type="text" autoFocus value={input} onChange={handleChange} />;
+}
+
+const StyledInput = styled.input`
+  font-weight: bold;
+  with: 400px;
+  height: 3em;
+  font-size: 22px;
+`;
+
+export default UserInput;
