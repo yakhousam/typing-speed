1218ea64f910238c43fc7bfc8c321a909238ad7b
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-17 13:00:36 +0100

    delete MainPage component and move all its logic to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer, useEffect, useRef, useState } from "react";
 import { createGlobalStyle } from "styled-components";
-
-import { Grid } from "./component/styledComponents";
-import MainPage from "./component/MainPage";
 import SideBare from "./component/SideBare";
+import Chart from "./component/Chart";
+import {
+  Grid,
+  Section,
+  UserInput,
+  TextBox,
+  Main,
+  ButtonReload,
+  ChartsContainer,
+  ToolTip
+} from "./component/styledComponents";
+
+import {
+  setInput,
+  incrementCursor,
+  addError,
+  addErrorTxt,
+  removeError,
+  reload,
+  updateDisplayTxt,
+  decTimer,
+  startTimer,
+  gameOver,
+  updateDataChart,
+  getNewTxt
+} from "./actions";
+import {
+  checkInput,
+  calcAccuracy,
+  saveResultLocalStorage,
+  getOffsetTop
+} from "./utils";
+import { initState } from "./store";
+import reducer from "./reducer";
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -16,15 +47,177 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const [score, setScore] = useState(0);
   const [accuracy, setAccuracy] = useState(100);
-  const [timer, setTimer] = useState(60);
+  const [state, dispatch] = useReducer(reducer, initState);
+  const [offsetTop, setOffsetTop] = useState(0);
+  const [toolTip, setToolTip] = useState({
+    visible: false,
+    top: 0,
+    left: 0,
+    txt: ""
+  });
+  const textBoxRef = useRef();
+  const {
+    input,
+    cursor,
+    displayText,
+    textArr,
+    errorArr,
+    errorArrTxt,
+    timer,
+    isTimerStarted,
+    score,
+    dataChart
+  } = state;
+
+  const handleChange = e => {
+    if (timer < 1 || cursor > textArr.length - 1) {
+      return;
+    }
+
+    if (!isTimerStarted && timer > 0) {
+      startTimer(true, dispatch);
+    }
+    let input = e.target.value;
+    if (!checkInput(input, textArr[cursor])) {
+      if (!errorArr.includes(cursor)) {
+        addError(dispatch, input);
+      }
+    } else {
+      if (errorArr.includes(cursor)) {
+        removeError(dispatch);
+      }
+    }
+    if (input.endsWith(" ")) {
+      if (errorArr.includes(cursor)) {
+        addErrorTxt(dispatch, { id: cursor, input });
+      }
+      incrementCursor(dispatch);
+      input = "";
+
+      const offset = getOffsetTop(textBoxRef);
+      if (offset > offsetTop) {
+        if (offsetTop > 0) {
+          textBoxRef.current.scroll({
+            top: textBoxRef.current.scrollTop + (offset - offsetTop),
+            behavior: "smooth"
+          });
+        }
+        setOffsetTop(offset);
+      }
+    }
+    setInput(input, dispatch);
+  };
+
+  useEffect(() => {
+    if (timer > 0) {
+      updateDisplayTxt(dispatch);
+    }
+  }, [cursor, errorArr, input, timer]);
+
+  useEffect(() => {
+    if (timer > 0 && isTimerStarted) {
+      const interval = setInterval(() => {
+        decTimer(dispatch);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+    if (timer < 1 && isTimerStarted) {
+      gameOver(dispatch);
+    }
+  }, [timer, isTimerStarted]);
+
+  useEffect(() => {
+    if (timer < 1 && score > 0) {
+      const data = saveResultLocalStorage({ score, errorArr });
+      console.log("data =", data);
+      updateDataChart(data, dispatch);
+
+      setAccuracy(calcAccuracy({ score, errorArr }));
+
+      setOffsetTop(0);
+    }
+  }, [score, errorArr, timer, setAccuracy]);
+  useEffect(() => {
+    textBoxRef.current.scroll(0, 0);
+  }, []);
   return (
     <>
       <GlobalStyle />
       <Grid>
-        <MainPage setScore={setScore} setAccuracy={setAccuracy} setTimer={setTimer} />
         <SideBare score={score} accuracy={accuracy} timer={timer} />
+        <Main>
+          <button
+            onClick={() => {
+              getNewTxt(dispatch);
+            }}
+          >
+            New text
+          </button>
+          <TextBox
+            id="textBox"
+            ref={textBoxRef}
+            height="5em"
+            onMouseOut={() => {
+              setToolTip({ ...toolTip, visible: false });
+            }}
+            onMouseOver={e => {
+              const { id = 20000 } = e.target;
+              const error = errorArrTxt.find(el => el.id === +id);
+              if (error) {
+                setToolTip({
+                  ...toolTip,
+                  visible: true,
+                  txt: error.input
+                });
+              }
+            }}
+            onMouseMove={e => {
+              const left = e.clientX;
+              const top = e.clientY;
+              if (toolTip.visible) {
+                setToolTip({ ...toolTip, left, top });
+              }
+            }}
+          >
+            {displayText}
+          </TextBox>
+          <Section>
+            <UserInput
+              type="text"
+              autoFocus
+              value={input}
+              onChange={handleChange}
+            />
+            {timer < 1 && (
+              <ButtonReload
+                onClick={() => {
+                  reload(dispatch);
+
+                  setAccuracy(100);
+                  textBoxRef.current.scroll({
+                    top: 0,
+                    left: 0,
+                    behavior: "smooth"
+                  });
+                }}
+              />
+            )}
+          </Section>
+        </Main>
+        {true && (
+          <ChartsContainer>
+            <Chart
+              title="WPM"
+              data={dataChart.map(d => ({ date: d.date, value: d.score }))}
+            />
+            <Chart
+              title="Accuracy"
+              data={dataChart.map(d => ({ date: d.date, value: d.accuracy }))}
+            />
+          </ChartsContainer>
+        )}
+        <ToolTip {...toolTip}>{toolTip.txt}</ToolTip>
       </Grid>
     </>
   );
