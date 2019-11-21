36c5d8855d99ec8c44adf2c5da1cbbff81cb5a7f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-21 11:59:16 +0100

    added styles to UserInput

--- src/component/UserInput.js
+++ src/component/UserInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import {
   setInput,
@@ -11,7 +11,7 @@ import {
 } from "../actions";
 import { checkInput } from "../utils";
 
-const UserInput = (props) =>  {
+const UserInput = props => {
   const { dispatch } = props;
   const {
     textArr,
@@ -22,74 +22,80 @@ const UserInput = (props) =>  {
     errorArr
   } = props.state;
   const handleChange = e => {
-    let newErroArr = [...errorArr]
-    let newCursor = cursor
+    let newErroArr = [...errorArr];
+    let newCursor = cursor;
     if (timer < 1 || cursor > textArr.length - 1) {
       return;
     }
     if (!isTimerStarted && timer > 0) {
-      startTimer({start: true, dispatch});
+      startTimer({ start: true, dispatch });
     }
     let input = e.target.value;
-    console.log('input =', input)
+    console.log("input =", input);
     if (!checkInput(input, textArr[cursor])) {
       if (!errorArr.includes(cursor)) {
         // addError({dispatch, error: cursor});
-        newErroArr.push(cursor)
+        newErroArr.push(cursor);
       }
     } else {
       if (errorArr.includes(cursor)) {
         // removeError({dispatch, errorArr, error: cursor});
-        newErroArr = newErroArr.filter(err => err !== cursor)
+        newErroArr = newErroArr.filter(err => err !== cursor);
       }
     }
     if (input.endsWith(" ")) {
       if (newErroArr.includes(cursor)) {
-        addErrorTxt({dispatch, error: { id: cursor, input }});
+        addErrorTxt({ dispatch, error: { id: cursor, input } });
       }
-      incrementCursor({dispatch});
+      incrementCursor({ dispatch });
       input = "";
-      newCursor++
+      newCursor++;
     }
-    setInput({input, dispatch, });
-    updateDisplayTxt({dispatch, state: {
-      ...props.state, 
-      input,
-      errorArr: [...newErroArr],
-      cursor: newCursor
-    }})
+    setInput({ input, dispatch });
+    updateDisplayTxt({
+      dispatch,
+      state: {
+        ...props.state,
+        input,
+        errorArr: [...newErroArr],
+        cursor: newCursor
+      }
+    });
   };
-  const [isFocus, setIsFocus] = useState(false)
-  return <StyledInput 
-  type="text" 
-  placeholder={!isFocus && timer > 0 ? "Type your text here...!" : ''}
-  autoFocus 
-  value={input} 
-  onChange={handleChange} 
-  onFocus={() => {
-    setIsFocus(true)
-  }}
-  onBlur={() => {
-    setIsFocus(false)
-  } }
-  isFocus={isFocus}
-  />;
-}
+  const [isFocus, setIsFocus] = useState(false);
+  return (
+    <StyledInput
+      type="text"
+      placeholder={!isFocus && timer > 0 ? "Type your text here...!" : ""}
+      autoFocus
+      value={input}
+      onChange={handleChange}
+      onFocus={() => {
+        setIsFocus(true);
+      }}
+      onBlur={() => {
+        setIsFocus(false);
+      }}
+      isFocus={isFocus}
+    />
+  );
+};
 
 const StyledInput = styled.input`
-  font-weight: bold;
   width: 400px;
-  height: 3em;
-  font-size: 22px;
-  background: none;
+  background: transparent;
   text-align: center;
-  padding-top: 1em;
-  
-  ${props => props.isFocus ? `
-    border: none;
-    border-bottom: thin solid green;
-  ` : `
-    border: thin dashed green;
+  font-size: 24px;
+  padding: .2em .5em;
+
+  ${props =>
+    props.isFocus
+      ? `
+    border: thin solid transparent;
+    border-bottom: thin solid #6148ff;
+  `
+      : `
+    border: thin dashed #6148ff;
   `}
 `;
 
